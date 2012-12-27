ff14597280cef71e7571bd010918f6c50d542b4a
James Huston <spam@jameshuston.net>
2012-12-27 12:20:35 -0500

    added basic url parsing to htmlPostHandler

--- test/unit/serverHandlers/httpPostHandler.test.js
+++ test/unit/serverHandlers/httpPostHandler.test.js
@@ -3,17 +3,17 @@ var expect = require("expect.js");
 var url = require("url");
 
 describe("When created an instance of a handler", function() {
-  it("should create a real instance", function() {
-    var testRequest = new Sprocket.Request({}, "test123");
-    var endpoint = "http://localhost-services.redventures.net/queue.php";
-    var handler = new Sprocket.HttpPostEndpoint(testRequest, endpoint);
+  var testRequest = new Sprocket.Request({}, "test123");
+  var endpoint = "http://localhost-services.redventures.net/queue.php";
+  var handler = new Sprocket.HttpPostEndpoint(testRequest, endpoint);
 
+  it("should create a real instance", function() {
     expect(handler).to.be.a(Sprocket.HttpPostEndpoint);
     expect(handler.request).to.equal(testRequest);
+  });
+
+  it("should properly parse a basic test url", function() {
     expect(handler.endpointUrl).to.equal(endpoint);
-
-    console.log(handler);
-
     expect(handler.url.port).to.equal(80);
     expect(handler.postUrl).to.equal("http://localhost-services.redventures.net");
   });
